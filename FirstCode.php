a232ec2976fe89f8577395e01eda183ff4387a1f
Alua Otezhan <190103411@stu.sdu.edu.kz>
2021-02-09 20:39:16 +0600

    First code in PHP

---
+++ FirstCode.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$l = array("Kazakh", "Russian", "Turkish", "English");
+
+for($i=0; $i<count($l); $i++){
+    echo "I know ".$l[$i]." language.<br>";
+}
+?>
+
+</body>
+</html>
