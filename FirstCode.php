069b19d019e2d57fef72176b36f2b8385c8f427d
Alua Otezhan <190103411@stu.sdu.edu.kz>
2021-02-09 21:10:12 +0600

    Add one more line.

--- FirstCode.php
+++ FirstCode.php
@@ -4,7 +4,7 @@
 
 <?php
 $l = array("Kazakh", "Russian", "Turkish", "English");
-echo "I know 4 languages. Let me show them using for loop.";
+echo "Hello! I know 4 languages. Let me show them using for loop.";
 for($i=0; $i<count($l); $i++){
     echo "I know ".$l[$i]." language.<br>";
 }
